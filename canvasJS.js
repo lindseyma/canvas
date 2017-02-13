2b54c97b8347af3135f7e4838a809d683197cb52
IAsWell <jyang1737@gmail.com>
2017-02-13 15:24:23 -0500

    finished rectangle clicking

--- canvasJS.js
+++ canvasJS.js
@@ -1,7 +1,17 @@
+var canvas = document.getElementById("slate");
+var ctx = canvas.getContext("2d");
 var b = document.getElementById("b");
+
 b.addEventListener("click", f);
-var ctx=c.getContext('2d');
 
 var f = function(){
-	ctx.clearRect(0,0,500,300);
+    ctx.clearRect(0,0,500,300);
 }
+
+canvas.addEventListener("click", function(e) {
+    mousex = e.clientX;
+    mousey = e.clientY;
+    ctx.fillStyle="#002366";
+    ctx.fillRect(mousex, mousey, 50, 100);
+});
+
