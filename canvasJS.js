563035c04415fa4a3af76ef46de8665c910ca626
Lindsey Ma <lma2@stuy.edu>
2017-02-14 12:38:03 -0500

    dots doesn't work

--- canvasJS.js
+++ canvasJS.js
@@ -1,16 +1,35 @@
 var canvas = document.getElementById("slate");
 var ctx = canvas.getContext("2d");
 var b = document.getElementById("b");
+ctx.beginPath();
 
 b.addEventListener("click", function(e){
     console.log("clear button");
     ctx.clearRect(0,0,300,500);
     });
 
-canvas.addEventListener("click", function(e) {
+/*canvas.addEventListener("click", function(e) {
     mousex = e.clientX;
     mousey = e.clientY;
     ctx.fillStyle="#002366";
     ctx.fillRect(mousex, mousey, 50, 100);
 });
+*/
 
+canvas.addEventListener("click", dots);
+
+var dots = function(e){
+	console.log("dots");
+	mousex=e.clientX;
+	mousey= e.clientY;
+	ctx.moveTo(X,Y);
+	ctx.arc(mousex, mousey, 10, 0, 2*Math.PI);
+	stroke();
+}
+	
+var rect = function(e){
+    mousex = e.clientX;
+    mousey = e.clientY;
+    ctx.fillStyle="#002366";
+    ctx.fillRect(mousex, mousey, 50, 100);
+};
