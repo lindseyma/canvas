6a2a323a0261170e17f55fba3cdc76d7c837ed82
IAsWell <jyang1737@gmail.com>
2017-02-14 19:59:53 -0500

    merge

--- canvasJS.js
+++ canvasJS.js
@@ -5,12 +5,15 @@ var b = document.getElementById("b");
 b.addEventListener("click", function(e){
     console.log("clear button");
     ctx.clearRect(0,0,300,500);
-    });
+});
 
+
+
+/*
 canvas.addEventListener("click", function(e) {
     mousex = e.clientX;
     mousey = e.clientY;
     ctx.fillStyle="#002366";
     ctx.fillRect(mousex, mousey, 50, 100);
-});
-
+});				
+*/
